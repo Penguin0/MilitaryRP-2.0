575683575e19a690dd3530a1f28e956a1a80051c
Penguin <unitedarms123@gmail.com>
2016-02-20 12:12:44 -0500

    Create ammotypes.lua

---
+++ ammotypes.lua
@@ -0,0 +1,82 @@
+/*---------------------------------------------------------------------------
+/*---------------------------------------------------------------------------
+Ammo types
+---------------------------------------------------------------------------
+Ammo boxes that can be purchased in the F4 menu.
+
+--[[Add your custom ammo types in this file. Here's the syntax:
+DarkRP.createAmmoType("ammoType", {
+    name = "Ammo name",
+    model = "Model",
+    price = 1234,
+    amountGiven = 5678,
+    customCheck = function(ply) return ply:IsAdmin() end
+})
+
+ammoType: The name of the ammo that Garry's mod recognizes
+	If you open your SWEPs shared.lua, you can find the ammo name next to
+	SWEP.Primary.Ammo = "AMMO NAME HERE"
+	or
+	SWEP.Secondary.Ammo = "AMMO NAME HERE"
+
+name: The name you want to give to the ammo. This can be anything.
+
+model: The model you want the ammo to have in the F4 menu
+
+price: the price of your ammo in dollars
+
+amountGiven: How much bullets of this ammo is given every time the player buys it
+
+customCheck: (Optional! Advanced!) a Lua function that describes who can buy the ammo.
+	Similar to the custom check function for jobs and shipments
+	Parameters:
+		ply: the player who is trying to buy the ammo
+
+Examples are below!]]
+---------------------------------------------------------------------------*/
+
+-- Pistol ammo type. Used by p228, desert eagle and all other pistols
+DarkRP.createAmmoType("pistol", {
+	name = "Pistol ammo",
+	model = "models/Items/BoxSRounds.mdl",
+	price = 50,
+	amountGiven = 48
+})
+
+-- Buckshot ammo, used by the shotguns
+DarkRP.createAmmoType("buckshot", {
+	name = "Shotgun ammo",
+	model = "models/Items/BoxBuckshot.mdl",
+	price = 100,
+	amountGiven = 48
+})
+
+DarkRP.createAmmoType("smg1", {
+	name = "SMG ammo",
+	model = "models/Items/BoxMRounds.mdl",
+	price = 80,
+	amountGiven = 60
+})
+
+DarkRP.createAmmoType("ar2", {
+	name = "Rifle ammo",
+	model = "models/Items/BoxMRounds.mdl",
+	price = 120,
+	amountGiven = 60
+})
+
+DarkRP.createAmmoType("357", {
+	name = ".357 ammo",
+	model = "models/Items/BoxMRounds.mdl",
+	price = 20,
+	amountGiven = 12
+})
+
+DarkRP.createCategory{
+    name = "Other",
+    categorises = "ammo",
+    startExpanded = true,
+    color = Color(0, 107, 0, 255),
+    canSee = fp{fn.Id, true},
+    sortOrder = 255,
+}
